0f7b1292892a848c18512404e2501939c8898adf
zhichao-aws <zhichaog@amazon.com>
2023-12-26 14:30:12 +0800

    add swapoff
    Signed-off-by: zhichao-aws <zhichaog@amazon.com>

--- opensearch/cluster_management/prepare_env.py
+++ opensearch/cluster_management/prepare_env.py
@@ -17,6 +17,7 @@ async def prepare_env(configs):
             aws s3 cp s3://neural-sparse/2.12.0-SNAPSHOT.tar.gz 2.12.0-SNAPSHOT.tar.gz
             tar -xzvf 2.12.0-SNAPSHOT.tar.gz > /dev/null
 
+            sudo swapoff -a
             echo 'vm.max_map_count=262144' | sudo tee /etc/sysctl.conf
             sudo sysctl -p
             ls
